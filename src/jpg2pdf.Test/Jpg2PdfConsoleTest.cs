0c397e85182ffcb16afd878919a73abfc50460ad
lucaquaglio <MCMpadova01>
2023-03-01 21:38:38 +0100

    Refactoring unit tests

---
+++ src/jpg2pdf.Test/Jpg2PdfConsoleTest.cs
@@ -0,0 +1,11 @@
+﻿namespace jpg2pdf.Test
+{
+	sealed class Jpg2PdfConsoleTest
+	{
+		[Test]
+		public void TestConvertFile()
+		{
+
+		}
+	}
+}
---
+++ src/jpg2pdf.Test/TestHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace jpg2pdf.Test
+{
+	static class TestHelper
+	{
+		public static string GetPdfFileNameGivenImageFileName(string imageFilename) => $"{imageFilename}.pdf";
+
+		public static void WriteFileGivenResourceName(string resourceName)
+		{
+			using var resourceStream = GetResourceStream(resourceName);
+			using var fileStream = new FileStream(resourceName, FileMode.Create);
+			resourceStream.CopyTo(fileStream);
+		}
+
+		public static Stream GetResourceStream(string resourceName)
+		{
+			return Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName) ?? throw new InvalidOperationException();
+		}
+
+		public const string Test1ResourceName = "jpg2pdf.Test.TestFiles.Test1.jpg";
+		public const string Test2ResourceName = "jpg2pdf.Test.TestFiles.Test2.jpg";
+		public const string Test3ResourceName = "jpg2pdf.Test.TestFiles.Test3.jpg";
+	}
+}
