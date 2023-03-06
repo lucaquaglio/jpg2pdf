409e40b3c82888c0523f38c9fcd0f19ce1b3c94b
lucaquaglio <MCMpadova01>
2023-03-06 20:56:00 +0100

    Added
    -TestMain_OutputFilenameSpecified
    -TestMain_WithMultipleFiles_NoOutputFilenameSpecified
    -TestMain_WithMultipleFile_OutputFilenameSpecified

--- src/jpg2pdf.Test/Jpg2PdfConsoleTest.cs
+++ src/jpg2pdf.Test/Jpg2PdfConsoleTest.cs
@@ -11,6 +11,15 @@
 			Assert.That(File.Exists(expectedOutputFilename), Is.True);
 		}
 
+		[Test, TestCaseSource(nameof(GetTestDataResourceFileNames))]
+		public void TestMain_OutputFilenameSpecified(string resourceName)
+		{
+			var outputFilename = "file.pdf";
+
+			Console.Program.Main(new[] { resourceName, outputFilename });
+			Assert.That(File.Exists(outputFilename), Is.True);
+		}
+
 		[Test]
 		public void TestMain_WithNoArguments()
 		{
@@ -22,6 +31,30 @@
 		{
 			Assert.Throws<InvalidOperationException>(() => Console.Program.Main(new[] { "A", "B" }));
 
+		}
+
+		[Test]
+		public void TestMain_WithMultipleFiles_NoOutputFilenameSpecified()
+		{
+			var imageFileNameCollection = GetTestDataResourceFileNames();
+			var expectedOutputFilename = TestHelper.GetPdfFileNameGivenImageFileName(imageFileNameCollection.ElementAt(0));
+
+			Console.Program.Main(imageFileNameCollection.ToArray());
+
+			Assert.That(File.Exists(expectedOutputFilename), Is.True);
+		}
+
+		[Test]
+		public void TestMain_WithMultipleFile_OutputFilenameSpecified()
+		{
+			var args = GetTestDataResourceFileNames().ToList();
+			var outputOutputFilename = "Result.pdf";
+
+			args.Add($"-o {outputOutputFilename}");
+
+			Console.Program.Main(args.ToArray());
+
+			Assert.That(File.Exists(outputOutputFilename), Is.True);
 		}
 
 		[SetUp]
